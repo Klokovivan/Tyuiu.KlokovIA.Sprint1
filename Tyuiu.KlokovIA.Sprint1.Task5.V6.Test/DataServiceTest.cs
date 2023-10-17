2073621f0bd1555d82893bc5e4adf908e028cc32
ivan <ivan.proekt00@mail.ru>
2023-10-18 01:34:24 +0500

    Task5

---
+++ Tyuiu.KlokovIA.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KlokovIA.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.KlokovIA.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int n, k = 1;
+            var res = ds.Calculate(k);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
